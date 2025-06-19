86bd8bafc34948455e45c98b14be7f87d43a6a98
Elijah M <elijah.macharia@student.moringaschool.com>
2025-06-19 11:53:18 +0300

    Flask-code

--- server/app.py
+++ server/app.py
@@ -1,6 +1,16 @@
 from server import create_app
+from flask_restful import Api,Resource
+
 
 app = create_app()
+
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return {'message': 'Welcome to the Pizza Restaurant API'}
+    
+api.add_resource(Home, '/')    
 
 if __name__ == '__main__':
     app.run(debug=True)
